a757950be774274c004dc7e2fe18bd9c114ee341
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-08-16 20:31:27 +0300

    Added a support for opening a device and creating a device context.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,40 @@
+// ============================================================================
+//
+// Some useful resources:
+// http://openal.org/documentation/OpenAL_Programmers_Guide.pdf
+//
+// ============================================================================
+
+#include <cstdio>
+#include <cstdlib>
+
+#include <AL/al.h>
 #include <AL/alc.h>
 
 int main()
 {
+  // ==========================================================================
+  // OPEN A DEVICE
+  // The first thing to do in OpenAL is to open a device (nullptr = default).
+  // ==========================================================================
   auto device = alcOpenDevice(nullptr);
+  if (device == nullptr) {
+    printf("alcOpenDevice failed: Unable to open device.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // ==========================================================================
+  // CREATE A CONTEXT
+  // The second thing is to create a context for the sound device.
+  // ==========================================================================
+  auto ctx = alcCreateContext(device, nullptr);
+  if (ctx == nullptr) {
+    printf("alcCreateContext failed: Unable to create device context.\n");
+    alcCloseDevice(device);
+    exit(EXIT_FAILURE);
+  }
+
+  alcDestroyContext(ctx);
   alcCloseDevice(device);
   return 0;
 }
